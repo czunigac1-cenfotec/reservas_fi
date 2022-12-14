25164ad7e0687fde50589932c91e7c85f65165c6
kkd <kkd@kkds-MacBook-Pro.local>
2022-12-13 22:54:01 -0600

    Main backend project structure

---
+++ backend/src/main/java/ac.cr/ucr/ReservasApplication.java
@@ -0,0 +1,17 @@
+package ac.cr.ucr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasApplication {
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasApplication.class, args);
+    }
+
+}
