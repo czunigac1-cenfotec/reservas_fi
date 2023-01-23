1b75cf896de72738e5af6fd2caaff5afed4473c1
josfallme <jfallasm1@ucenfotec.ac.cr>
2023-01-22 22:04:15 -0600

    LDAP connection and configuration added

---
+++ backend/src/main/java/ac/cr/ucr/model/UserLogin.java
@@ -0,0 +1,33 @@
+package ac.cr.ucr.model;
+
+public class UserLogin {
+    
+    private String userName;
+    private String password;
+
+    public UserLogin() {
+        this.userName = "";
+        this.password = "";
+    }
+
+    public UserLogin(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
