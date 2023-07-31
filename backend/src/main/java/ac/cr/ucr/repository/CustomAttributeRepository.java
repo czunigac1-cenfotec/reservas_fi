96273f938de9dc6c4ffe79c6e0ad9f90646127f8
czunigac1_cenfotec <kkd@mbp.local>
2023-07-31 01:18:33 -0600

    CustomAttributeByRoomUuid

--- backend/src/main/java/ac/cr/ucr/repository/CustomAttributeRepository.java
+++ backend/src/main/java/ac/cr/ucr/repository/CustomAttributeRepository.java
@@ -1,11 +1,16 @@
 package ac.cr.ucr.repository;
 
 import ac.cr.ucr.model.CustomAttribute;
+import ac.cr.ucr.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CustomAttributeRepository extends JpaRepository<CustomAttribute, UUID> {
+    @Query("SELECT r FROM CustomAttribute r WHERE r.roomAvailabilityUuid = ?1")
+    List<CustomAttribute> findByRoomAvailabilityUuid(UUID roomAvailabilityUuid);
 }
