8f74c797c9f68ab476f6ed2198519738a2621c09
Jose Fallas <jfallasm1@ucenfotec.ac.cr>
2023-10-12 18:15:56 -0600

    CORS annotation added

--- backend/src/main/java/ac/cr/ucr/controller/UserLoginController.java
+++ backend/src/main/java/ac/cr/ucr/controller/UserLoginController.java
@@ -9,6 +9,7 @@ import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import ac.cr.ucr.model.UserLoginResponse;
 import ac.cr.ucr.util.PropertyManager;
 
 @RequestMapping("/userLogin")
+@CrossOrigin
 @RestController
 public class UserLoginController {
     
